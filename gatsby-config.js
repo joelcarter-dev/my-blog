6623e12e148f03c79730ff9919d2ebff01387c6b
Dwaxy <joel.innes@gmail.com>
2018-07-14 14:13:01 +1200

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,22 @@ module.exports = {
     title: 'Strange Words Blog',
   },
   plugins: [
+  
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-100204166-2",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
+    
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
